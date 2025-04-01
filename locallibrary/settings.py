7eec15b23dc728a8d0e521bfa10ff6811ddeeb11
Artur <kazukevich.artur@gmail.com>
2025-04-01 21:09:02 +0300

    create catalog home page

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -63,7 +63,9 @@ ROOT_URLCONF = 'locallibrary.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, "locallibrary", "templates")
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
